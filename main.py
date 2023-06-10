31a5682ff42a9d60333d523abf6858aec28f2d93
SoumiOnGit <soumisarkar140902@gmail.com>
2023-06-10 13:40:42 +0530

    edits

---
+++ main.py
@@ -0,0 +1,8 @@
+noun = input("Noun : ")
+noun2 = input("Noun : ")
+adjective = input("Adjective : ")
+adjective2 = input("Adjective : ")
+verb = input("Verb : ")
+madlibs = "Once upon a time in a "+ adjective + " land, there was a brave " + noun + " named " + noun2 + " embarked on a " + adjective +" quest to "+ verb +" the legendary "+ noun2 + ". Along the way" + noun + " encountered a "+ adjective2 + noun + " who offered a " + noun + " has a reward for completing a " + adjective +" challenge." 
+
+print(madlibs)
